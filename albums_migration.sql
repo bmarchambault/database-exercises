48c963d75ac11dd20cfaf1842d575eaf7c01d3ed
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-07 11:51:36 -0500

    modified table order

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,11 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) DEFAULT 'Unknown',
     `name` VARCHAR(100),
-    release_date DATE,
-    sales FLOAT,
+    release_date SMALLINT UNSIGNED,
     genre VARCHAR(100),
+    sales FLOAT,
     PRIMARY KEY (id)
 );
+
+-- INSTRUCTOR USED:
+-- release_date SMALLINT UNSIGNED (instead of just date)

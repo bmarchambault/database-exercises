aa6f1ed4e7855746cd6b233961f1821a3bfa6507
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-07 11:53:41 -0500

    created quotes seeder file

--- quotes_seeder.sql
+++ quotes_seeder.sql
@@ -1,6 +1,11 @@
 USE quotes_db;
 
+-- doesn't delete the table just removing existing data inside the table
 TRUNCATE quotes;
+
+/*to run this:  mysql -u bobbiearchambault -p < quotes_seeder.sql
+SELECT * FROM quotes
+ */
 
 
 # This is an example of a single record being added to db
@@ -18,3 +23,9 @@ VALUES  ('Douglas', 'Adams', 'Don''t panic'),
 # This is an example of a default value being added
 INSERT INTO quotes (author_first_name, author_last_name, quote)
 VALUES (DEFAULT, 'Orsinger', 'TAKE THE LOOP OUT!');
+
+INSERT INTO quotes (author_last_name, quote)
+VALUES ('Iroh', "Sharing tea is fun!");
+
+INSERT INTO quotes (author_last_name, quote)
+VALUES ('unknown', "Never aurgue with an idiot.  They'll drag you down to their level, then beat you with experience.");

5ca57716088e52c492e8ca9c39046b8df3a508d2
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-10 09:43:25 -0500

    created function exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,11 +4,18 @@ SHOW TABLES;
 DESCRIBE employees;
 SELECT * FROM employees;
 
+# Copy the order_by_exercises.sql script and save it as functions_exercises.sql.
+#
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no DESC;
 
-SELECT * FROM employees Where first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name ;
-SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
 
-
-
-SELECT * FROM employees WHERE (hire_date LIKE '199%'
-    AND birth_date LIKE '%-12-25') ORDER BY birth_date, hire_date DESC;
+SELECT *, CONCAT('Days at company: ', DATEDIFF(NOW(), hire_date))
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
